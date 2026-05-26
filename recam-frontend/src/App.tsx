96b328c9374758fea8bec08382648a5aa1b1a2de
sivamw6 <105441569+sivamw6@users.noreply.github.com>
2026-05-27 02:49:45 +1000

    feat(RECAM-1309): Initialize React project with Vite, TailwindCSS v4, and folder structure

---
+++ recam-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <>
+        <div className="text-3xl font-bold text-blue-500">RECAM Frontend</div>
+
+    </>
+  );
+}
+
+export default App;
